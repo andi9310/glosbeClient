77adc638252ae98b2a0c062229936db8804abb0b
Andrzej Podgórski <andrzej.podgorski@hotmail.com>
2016-05-09 23:37:38 +0200

    initial

---
+++ glosbeClient/JSONs.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace glosbeClient
+{
+    [DataContract]
+    public class Response
+    {
+        [DataMember(Name = "result")]
+        public string Result;
+
+        [DataMember(Name = "tuc")]
+        public Tuc[] Tuc;
+    }
+
+    [DataContract(Name = "tuc")]
+    public class Tuc
+    {
+        [DataMember(Name = "phrase")]
+        public Phrase Phrase;
+    }
+
+    [DataContract(Name = "phrase")]
+    public class Phrase
+    {
+        [DataMember(Name = "text")]
+        public string Text;
+    }
+
+}
---
+++ glosbeClient/glosbeClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+
+namespace glosbeClient
+{
+    public class GlosbeClient
+    {
+        public IEnumerable<string> GetTranslations(string word)
+        {
+            var response = SendRequest(word);
+            if (response == null || response.Result != "ok")
+            {
+                return new string[] { };
+            }
+            return from tuc in response.Tuc where tuc.Phrase != null select tuc.Phrase.Text;
+        }
+
+        public Response SendRequest(string word)
+        {
+            var requestUrl = $"https://glosbe.com/gapi/translate?from=eng&dest=pol&format=json&phrase={word}&pretty=true";
+            try
+            {
+                var request = WebRequest.Create(requestUrl) as HttpWebRequest;
+                using (var response = request?.GetResponse() as HttpWebResponse)
+                {
+                    if (response != null && response.StatusCode != HttpStatusCode.OK)
+                        throw new Exception($"Server error (HTTP {response.StatusCode}: {response.StatusDescription}).");
+                    var jsonSerializer = new DataContractJsonSerializer(typeof(Response));
+                    var stream = response?.GetResponseStream();
+                    if (stream == null) return null;
+                    var objResponse = jsonSerializer.ReadObject(stream);
+                    var jsonResponse = objResponse as Response;
+                    return jsonResponse;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
